2f0872bd6508be12970064dbf708df32a2c8d3d4
Adenildo Junior <adenildojunior52@gmail.com>
2019-04-10 13:09:06 -0300

    Delete open.js

--- joao/components/open.js
+++
@@ -1,3 +0,0 @@
-
-
-//Falta fazer um concensso entre submit e button, pq quando você usa type="button" ele só chama a função, e quando usa o type="submit" ele passa a validação, mas atualiza a página e não dá pra testar a função.
